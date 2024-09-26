348842e69520194a929e64b1fa49d3245c491489
JonathanT2024 <jonathan.ingenieriaindustrial@gmail.com>
2024-09-26 15:41:59 -0300

    hito4 - se agregan metodos String en administrador

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from main.models import Comuna, Inmueble, UserProfile
 
 # Register your models here.
+class ComunaAdmin(admin.ModelAdmin):
+    pass
+
+class InmuebleAdmin(admin.ModelAdmin):
+    pass
+
+class UserProfileAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Comuna, ComunaAdmin)
+admin.site.register(Inmueble, InmuebleAdmin)
+admin.site.register(UserProfile, UserProfileAdmin)
+
+admin.site.site_header = "Arriendos.cl"
+admin.site.index_title = "Bienvenidos al portal de administración de Arriendos.cl"
